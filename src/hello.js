d80a0b8c0083223b14250569cdbe48cfbda78d40
Nipun2508 <130894142+Nipun2508@users.noreply.github.com>
2023-12-15 19:33:53 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From Nipun!",
+        message: "Hello From Nipun's App!",
       },
       null,
       2
